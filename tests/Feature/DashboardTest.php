f7943fff23ecf425b617357faf28ddbef8b5dbd6
MarkDev232 <marksauquillo3@gmail.com>
2025-06-07 10:19:58 +0800

    Fix(Dashboard-Test): fixing Dashboard Test

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use App\Models\TaskList;
 
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
@@ -8,8 +9,11 @@ test('guests are redirected to the login page', function () {
     $this->get('/dashboard')->assertRedirect('/login');
 });
 
+
 test('authenticated users can visit the dashboard', function () {
     $this->actingAs($user = User::factory()->create());
+
+    TaskList::factory()->create(['user_id' => $user->id]);
 
     $this->get('/dashboard')->assertOk();
 });
